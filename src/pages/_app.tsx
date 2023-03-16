87bae1cedec3636b0a02773a9bb65d2d3ccd7b9c
Marcelo Barbosa <marcelo.castilho2707@gmail.com>
2023-03-16 11:58:25 -0300

    feat: adding styled-components config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from '@/styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>React Avançado - BoilerPlate</title>
+        <link rel='shortcut icon' href='/favicon.ico'/>
+        <meta
+          name='description'
+          content='A simple project'
+        />
+      </Head>
+      <GlobalStyles/>
+      <Component {...pageProps} />
+    </>
+  )
 }
